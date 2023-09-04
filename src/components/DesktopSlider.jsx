00d794f943c74e7b9dfd12eb9631548b9d05929e
Mark Carrington <frumarkcarrington@gmail.com>
2023-09-04 17:08:58 +0530

    added the chatbot api for the react file and created the necessary components for the project

---
+++ src/components/DesktopSlider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DesktopSlider() {
+  return <div>DesktopSlider</div>;
+}
+
+export default DesktopSlider;
---
+++ src/components/MobileSlider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MobileSlider() {
+  return <div>MobileSlider</div>;
+}
+
+export default MobileSlider;

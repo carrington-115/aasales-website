1d0e1b9cc396234489483e7aa33dd448610452c2
Mark Carrington <frumarkcarrington@gmail.com>
2023-09-05 11:28:35 +0530

    added tailwind to the project

--- src/utilities/Chat.jsx
+++ src/utilities/Chat.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
+import styled from "styled-components";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
+const propertyID = import.meta.env.VITE_PRODUCT_ID;
+const widjetID = import.meta.env.WIDJET_ID;
 
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <Container>
+      <div
+        className="chat-btn"
+        style={{
+          width: "60px",
+          height: "60px",
+        }}
+      >
+        <BsFillChatQuoteFill
+          color="blue"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </div>
+      <ChatComponent />
+    </Container>
+  );
 }
 
+const ChatComponent = () => {
+  const tawkMessengerRef = useRef();
+  return (
+    <TawkMessengerReact
+      ref={tawkMessengerRef}
+      propertyId="64f23b20b2d3e13950ed6b26"
+      widjetId="1h993dum6"
+    />
+  );
+};
+
 export default Chat;
+
+const Container = styled.section`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  .chat-btn {
+    cursor: pointer;
+    &:hover {
+      svg {
+        fill: red;
+      }
+    }
+  }
+`;

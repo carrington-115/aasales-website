a388364a6b182613e7bb99a7af5fb6c91cbcdba2
Mark Carrington <frumarkcarrington@gmail.com>
2023-09-09 02:10:44 +0530

    started the products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
 function Products() {
-  return <div>Products</div>;
+  return (
+    <Container>
+      <PageHeader></PageHeader>
+    </Container>
+  );
 }
 
+const PageHeader = () => {
+  return <HeaderContainer></HeaderContainer>;
+};
+
 export default Products;
+
+const Container = styled.div``;
+
+const HeaderContainer = styled.section``;
